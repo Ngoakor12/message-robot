4f95c18c8811ebfb68faae8ee58fae21a315eba9
ngoakor12 <ngoakor12@gmail.com>
2022-01-30 08:22:22 +0200

    style: still styling basic frontend

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import "./App.css";
 import users from "./mock-data";
 
+import { useState } from "react";
+
 function App() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  console.log(isMenuOpen);
+  function toggleIsMenuOpen() {
+    setIsMenuOpen((prevState) => !prevState);
+  }
+
   const currentUser = users.find((user) => user.name === "Ngoako");
-  // console.log(currentUser);
   return (
     <div className="App">
       <header className="main-header">
-        <span className="menu-icon">🍔</span>
+        <span className="menu-icon" onClick={toggleIsMenuOpen}>
+          🍔
+        </span>
         <span className="menu-avatar">👦🏾</span>
       </header>
       <section className="main-content">
-        <nav className="nav">
+        <nav className={`nav ${!isMenuOpen && "closed"}`}>
           <ul>
             <li className="nav-item">
               <a href="#">
@@ -48,13 +58,23 @@ function App() {
               <h1 className="heading-title">Today</h1>
               <span>23 Jan Mon</span>
             </div>
-            <button className="schedule-message">Schedule message</button>
+            <button className="schedule-message">
+              <span className="schedule-message-icon">📝</span>Schedule message
+            </button>
           </header>
           <section className="messages">
             {currentUser.messages.map((message) => {
               return (
                 <div className="message" key={message.messageId}>
-                  <h2>{message.subject}</h2>
+                  <div className="message-state-icon">✅</div>
+                  <div className="message-summary">
+                    <h2>{message.subject}</h2>
+                    <div className="message-date-time">
+                      <span>⌚ {message.time}</span>
+                      {"   "}
+                      <span>📅 {message.date}</span>
+                    </div>
+                  </div>
                 </div>
               );
             })}
